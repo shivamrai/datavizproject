b4fad3f68915e3bf497e2bd2501e68f2ac4b24e2
Shivam Rai <shivam.rai.111@gmail.com>
2020-04-15 18:43:05 -0700

    added dropdown input to Pie chart

--- us-accidents-data-vis/client/src/components/PieChart.jsx
+++ us-accidents-data-vis/client/src/components/PieChart.jsx
@@ -1,10 +1,20 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, state ,setState} from "react";
 import ReactDOM from "react-dom";
 import * as d3 from "d3";
 import PieHooks from "./PieHooks";
 import "./styles.css";
+import Select from '@material-ui/core/Select';
+import { makeStyles, FormControl, InputLabel } from "@material-ui/core";
+
+const useStyles = makeStyles((theme) => ({
+  formControl: {
+    margin: theme.spacing(1),
+    minWidth: 120,
+  },
+}));
 
 export default function PieChart() {
+  const classes = useStyles();
   const generateData = (value, length = 2) =>
     d3.range(length).map((item, index) => ({
       date: index,
@@ -12,8 +22,17 @@ export default function PieChart() {
     }));
 
   const [data, setData] = useState(generateData(0));
+  const [state, setState] = React.useState({age: '',});
   const changeData = () => {
     setData(generateData());
+  };
+
+  const handleChange = (event) => {
+    const name = event.target.name;
+    setState({
+      ...state,
+      [name]: event.target.value,
+    });
   };
 
   useEffect(() => {
@@ -26,7 +45,26 @@ export default function PieChart() {
         <button onClick={changeData}>Transform</button>
       </div>
       <div>
-        <span className="label">Hooks</span>
+      <FormControl className={classes.formControl}>
+        <InputLabel htmlFor="age-native-simple">Age</InputLabel>
+        <Select
+          native
+          value={state.age}
+          onChange={handleChange}
+          inputProps={{
+            name: 'age',
+            id: 'age-native-simple',
+          }}
+        >
+          <option aria-label="None" value="" />
+          <option value={10}>Ten</option>
+          <option value={20}>Twenty</option>
+          <option value={30}>Thirty</option>
+        </Select>
+      </FormControl>
+      </div>
+      <div>
+        <span className="label">Hooks</span>  
         <PieHooks
           data={data}
           width={200}
